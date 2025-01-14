64e860e0d13e021684f3a29fffe3271e0bad65c3
Abbas Khan <ak9950025@gmail.com>
2025-01-14 19:41:11 +0500

    minor changes updated

--- backend/config/MongoDB.js
+++ backend/config/MongoDB.js
@@ -1,16 +1,12 @@
-import mongoose from 'mongoose'
-
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try
-    {
-        await mongoose.connect('mongodb+srv://mydemoforuser:demo500account25@cluster0.updwn.mongodb.net/?retryWrites=true');
-        console.log('DB Connected Successfully')
+    try {
+        await mongoose.connect('mongodb+srv://mydemoforuser:demo500account25@cluster0.updwn.mongodb.net/newsdb?retryWrites=true&w=majority');
+        console.log('DB Connected Successfully');
+    } catch (error) {
+        console.log('Error Connecting DB:', error);
     }
-    catch(error)
-    {
-        console.log("Error Connecting DB : ", error)
-    }
-}
+};
 
-export default connectDB
+export default connectDB;
